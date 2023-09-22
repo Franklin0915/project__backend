8dea6dc37f19ca353ba70c12c1230f7c26bae6a9
frank <franklinokrah465@gmail.com>
2023-09-22 14:26:25 +0000

    changed the registered name

--- account/models.py
+++ account/models.py
@@ -15,7 +15,7 @@ class RegisteredVehicle(models.Model):
     cars_image = models.ImageField(blank=True, null=True)
     owner_gender = models.CharField(max_length=100, choices = GENDER_LIST, blank=True, null=True)
     car_model = models.CharField(max_length=100, blank=True, null=True)
-    numberplate_state = models.BooleanField(default=False)
+    Registered = models.BooleanField(default=False)
     owners_nationality = models.CharField(max_length=255)
     owners_age = models.CharField(max_length=255)
     registration_expiry = models.CharField(max_length=255)
